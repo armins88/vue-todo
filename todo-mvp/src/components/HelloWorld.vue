40208eb9934d33d6d8449b3f844f6e5d2963bcdc
Armin S <armins88@gmail.com>
2020-06-20 12:16:49 -0400

    init store and components

---
+++ todo-mvp/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2> {{count}} </h2>
+
+
+
+    <button @click="increment"> + </button> 
+    <button @click="decrement"> - </button> 
+    
+  </div>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  computed: mapState({
+    count: state =>  state.count,
+  }),
+  methods: mapMutations([
+    'increment',
+    'decrement'
+  ])  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ todo-mvp/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+Vue.config.productionTip = false
+
+//Define store
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
+
+new Vue({
+  render: h => h(App),
+  store}).$mount('#app')
